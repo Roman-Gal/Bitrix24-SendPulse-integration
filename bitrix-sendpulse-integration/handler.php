72d60e7d99db1d6b63f1fe416b12075e40e112e7
Roman-Gal <roman.halaiko@gmail.com>
2022-08-04 19:33:38 +0200

    ready

---
+++ bitrix-sendpulse-integration/handler.php
@@ -0,0 +1,231 @@
+<?php
+require_once($_SERVER["DOCUMENT_ROOT"].'settings/crest.php');
+
+// file_put_contents($_SERVER['DOCUMENT_ROOT'].'/logs.txt',
+// "Parameters:"."\n".print_r($_REQUEST, true)."\n", FILE_APPEND);
+
+//get parametres and add them to variables
+$arDocument = $_REQUEST['document_id'];
+$paramCity = $_REQUEST['properties']['city'];
+$paramDeliveryAddress = $_REQUEST['properties']['delivery_address'];
+$paramPaymentMethodName = $_REQUEST['properties']['payment_method_name'];
+$paramPaymentMethodId = $_REQUEST['properties']['payment_method_id'];
+$paramOrderDate = $_REQUEST['properties']['order_date'];
+$paramPaymentStatus = $_REQUEST['properties']['payment_status'];
+$paramTtnNumber = $_REQUEST['properties']['ttn_number'];
+$paramTtnDate = $_REQUEST['properties']['ttn_date'];
+
+//checking document type (in our case LEAD or DEAL)
+if (is_array($arDocument))
+{
+    foreach ($arDocument as $param)
+    {// get document id
+        if (strpos($param, "DEAL_") === 0)
+        {
+            $deal_id = intval(substr($param,  strlen("DEAL_")));
+            break;
+        }
+        else if (strpos($param, 'LEAD_') === 0)
+        {
+            $lead_id = intVal(substr($param, strlen('LEAD_')));
+            break;
+        }
+    }
+}
+
+if ($lead_id) 
+{   
+    $currentLead = CRest::call('crm.lead.get',['id' => $lead_id]);
+
+    //robot will work only when LEAD STATUS_ID is NEW
+    if ($currentLead['result']['STATUS_ID'] == "NEW")
+    {
+        //get information about contact attached to LEAD
+        $currentContact = CRest::call('crm.contact.get',['id' => $currentLead['result']["CONTACT_ID"]]);
+        $currentContactEmail = $currentContact['result']["EMAIL"][0]["VALUE"];
+        $currentContactPhone = $currentContact['result']["PHONE"][0]["VALUE"];
+        $currentContactName = $currentContact['result']['NAME'] . " " . $currentContact['result']['LAST_NAME'];
+        $currentCity = $currentLead['result'][$paramCity];
+        $currentDeliveryAdres = $currentLead['result'][$paramDeliveryAddress];
+
+        //get payment methods in LEAD user forms     
+        $currentPaymentMethodId = $currentLead['result'][$paramPaymentMethodName];
+            $currentPaymentMethodFields = CRest::call('crm.lead.userfield.get',['ID' => $paramPaymentMethodId]);
+            foreach ($currentPaymentMethodFields['result']['LIST'] as $allPayMethods) {
+                if ($allPayMethods['ID'] == $currentPaymentMethodId) 
+                {
+                    $currentPaymentMethod = $allPayMethods['VALUE'];
+                }
+            }
+        //get order date
+        $currentOrderDate = date('Y-m-d',strtotime($currentLead['result'][$paramOrderDate]));
+
+        // get ordered products 
+         $allProductsInLead = CRest::call('crm.lead.productrows.get', ['ID' => $lead_id]);
+        
+         foreach($allProductsInLead['result'] as $product)
+         {   
+             //create new array of products to add into JSON array     
+             $productsArray[] = array(
+             "product_name" => $product['PRODUCT_NAME'],
+             "prodcut_amount" => $product['QUANTITY'],
+             "product_price" =>  $product['PRICE']
+             );
+         }
+        //get total products sum 
+        $currentSum = $currentLead['result']["OPPORTUNITY"];
+
+        //get payment status
+        $currentPaymentStatus = $currentLead['result'][$paramPaymentStatus]; 
+        
+        $document_id = $lead_id;
+    }
+} 
+else if ($deal_id) 
+{
+    //get current Deal
+    $currentDeal = CRest::call('crm.deal.get',['id' => $deal_id]);
+    
+    //get stage of deal
+    $stageId = $currentDeal['result']['STAGE_ID'];
+    
+    //get current Contact
+    $currentContact = CRest::call('crm.contact.get',['id' => $currentDeal['result']["CONTACT_ID"]]);
+  
+    //get current contact email 
+    $currentContactEmail = $currentContact['result']["EMAIL"][0]["VALUE"];
+    
+    //get current Phone number
+    $currentContactPhone = $currentContact['result']["PHONE"][0]["VALUE"];
+    
+    //get current Contact name
+    $currentContactName = $currentContact['result']['NAME'] . " " . $currentContact['result']['LAST_NAME'];
+    
+    //get current city
+    $currentCity = $currentDeal['result'][$paramCity];
+
+    //get current delivery adres
+    $currentDeliveryAdres = $currentDeal['result'][$paramDeliveryAddress];
+   
+    //get current payment method - need to add to params ID field and field name
+    //390 is an ID of userfield payment methods in DEAL user forms
+    $currentPaymentMethodId = $currentDeal['result'][$paramPaymentMethodName];
+    $currentPaymentMethodFields = CRest::call('crm.deal.userfield.get',['ID' => $paramPaymentMethodId]);
+    foreach ($currentPaymentMethodFields['result']['LIST'] as $allPayMethods) {
+        if ($allPayMethods['ID'] == $currentPaymentMethodId) 
+        {
+            $currentPaymentMethod = $allPayMethods['VALUE'];
+        }
+    }
+
+    //get order date -  need to add to params
+    $currentOrderDate = date('Y-m-d',strtotime($currentDeal['result'][$paramOrderDate]));
+ 
+    //get ordered products - no need to add to params
+    $allProductsInDeal = CRest::call('crm.deal.productrows.get', ['ID' => $deal_id]);
+     
+    foreach($allProductsInDeal['result'] as $product)
+    {     
+        //create new array of products to add into JSON array    
+        $productsArray[] = array(
+        "product_name" => $product['PRODUCT_NAME'],
+        "product_amount" => $product['QUANTITY'],
+        "product_price" =>  $product['PRICE']
+        );
+    }
+
+    //get payment status 
+    $currentPaymentStatus = $currentDeal['result'][$paramPaymentStatus];
+
+    //get total sum
+    $currentSum = $currentDeal['result']["OPPORTUNITY"];
+
+    //get ttn number
+    $currentTtnNumber = $currentDeal['result'][$paramTtnNumber];
+
+    //get ttn date
+    $currentTtnDate= $currentDeal['result'][$paramTtnDate];
+
+    $document_id = $deal_id;
+}
+
+ // Setup request to send json via POST raw
+ $phpToJson = array(
+    "email" => $currentContactEmail,
+    "phone" => $currentContactPhone,
+    "name" => $currentContactName,
+    "city" => $currentCity,
+    "delivery_address" => $currentDeliveryAdres,
+    "payment_method" => $currentPaymentMethod,
+    "order_date" => $currentOrderDate,
+    "order_id" => $document_id,
+    "products" => $productsArray,
+    "order_sum" => $currentSum,
+    "payment_status" => $currentPaymentStatus,
+    "ttn_number" => $currentTtnNumber,
+    "ttn_date" => $currentTtnDate,
+    );
+
+        // file_put_contents($_SERVER['DOCUMENT_ROOT'].'/logs.txt',
+        // "Array:"."\n" . print_r($phpToJson, true)."\n", FILE_APPEND);
+  
+
+if ($lead_id) {
+    $urlSendPulse = 'send_pulse_event_id';
+    sendJsonToSendPulse($urlSendPulse, $phpToJson);
+    
+}
+else if ($deal_id)
+{
+    switch ($stageId) 
+    { 
+        case '1': // Stage_1
+            $urlSendPulse = 'send_pulse_event_id';
+            sendJsonToSendPulse($urlSendPulse, $phpToJson);
+            break;
+        case '2': // Stage_2
+            $urlSendPulse = 'send_pulse_event_id';
+            sendJsonToSendPulse($urlSendPulse, $phpToJson);
+            break;
+        case '3': // Stage_3
+            $urlSendPulse = 'send_pulse_event_id';
+            sendJsonToSendPulse($urlSendPulse, $phpToJson);
+            break;
+        case '4': // Stage_4
+            $urlSendPulse = 'send_pulse_event_id';
+            sendJsonToSendPulse($urlSendPulse, $phpToJson);
+            break;
+        case '5': // Stage_5
+            $urlSendPulse = 'send_pulse_event_id';
+            sendJsonToSendPulse($urlSendPulse, $phpToJson);
+            break;
+        case '6': // Stage_6
+            $urlSendPulse = 'send_pulse_event_id';
+            sendJsonToSendPulse($urlSendPulse, $phpToJson);
+            break;    
+    }
+}
+
+function sendJsonToSendPulse($urlSendPulse, $phpToJson) 
+{ 
+    $curlInit = curl_init($urlSendPulse);
+    $sendPulseArray = json_encode($phpToJson);
+
+    curl_setopt($curlInit, CURLOPT_POSTFIELDS, $sendPulseArray);
+    curl_setopt($curlInit, CURLOPT_HTTPHEADER, array('Content-Type:application/json'));
+    curl_setopt($curlInit, CURLOPT_POST, true);
+    curl_setopt($curlInit, CURLOPT_SSL_VERIFYHOST, 0);
+    curl_setopt($curlInit, CURLOPT_SSL_VERIFYPEER, 0);    
+    curl_setopt($curlInit, CURLOPT_RETURNTRANSFER, true);
+    $result = curl_exec($curlInit);
+    
+    // file_put_contents( $_SERVER['DOCUMENT_ROOT'].'/logs.txt',
+    // "Curl:"."\n".print_r($result, true)."\n", FILE_APPEND);
+
+    curl_close($curlInit);
+}
+
+?>
+
+
+
---
+++ bitrix-sendpulse-integration/install.php
@@ -0,0 +1,177 @@
+<?php
+header('Content-Type: text/html; charset=UTF-8');
+$protocol = $_SERVER['SERVER_PORT'] == '443' ? 'https' : 'http';
+$host = explode(':', $_SERVER['HTTP_HOST']);
+$host = $host[0];
+define('BP_APP_HANDLER', $protocol.'://'.$host.explode('?', $_SERVER['REQUEST_URI'])[0]);
+$urlForRobot = $protocol.'://'.$host.explode('?', '/apps/robots/bitrix-sendpulse-integration/handler.php')[0];
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<title></title>
+</head>
+<body>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+<script src="//api.bitrix24.com/api/v1/"></script>
+<?if (!isset($_POST['PLACEMENT']) || $_POST['PLACEMENT'] === 'DEFAULT'):?>
+<h1>Robots Manager</h1>
+<div class="container-fluid">
+<div class="container-fluid">
+ 		<button onclick="installRobot();" class="btn btn-primary">Install</button>
+ 		<button onclick="uninstallRobot();" class="btn btn-danger">Delete</button>
+ 	</div>
+ 	<hr/>
+ 	<div class="container-fluid">
+ 		<button onclick="getList();" class="btn btn-light">Get list of installed robots</button>
+ 	</div>
+ </div>
+ <script type="text/javascript">
+ 	document.body.style.display = 'none';
+ 	BX24.init(function()
+ 	{
+ 		document.body.style.display = '';
+ 	});
+ 
+ 	function installRobot()
+ 	{
+ 		var params = {
+ 			'CODE': 'bitrix-sendpulse-integration',
+			'HANDLER': <?=json_encode($urlForRobot)?>,
+ 			'AUTH_USER_ID': 1,
+ 			'NAME': 'Selleris',
+ 			'USE_PLACEMENT': 'N',
+ 			'PLACEMENT_HANDLER': '<?=BP_APP_HANDLER?>',
+			 'PROPERTIES': {
+				'city': {
+					'Name': 'city',
+					'Type': 'string',
+				},
+				'delivery_address': {
+					'Name': 'delivery_address',
+					'Type': 'string',
+				},
+				'payment_method_name': {
+					'Name': 'payment_method_name',
+					'Type': 'string',
+				},
+				'payment_method_id': {
+					'Name': 'payment_method_id',
+					'Type': 'string',
+				},
+				'order_date': {
+					'Name': 'order_date',
+					'Type': 'string',
+				},
+				'payment_status': {
+					'Name': 'payment_status',
+					'Type': 'string',
+				},
+				'ttn_number': {
+					'Name': 'ttn_number',
+					'Type': 'string',
+				},
+				'ttn_date': {
+					'Name': 'ttn_date',
+					'Type': 'string',
+				},
+			}
+ 		};
+ 
+ 		BX24.callMethod(
+ 			'bizproc.robot.add',
+ 			params,
+ 			function(result)
+ 			{
+ 				if(result.error())
+ 					alert("Error: " + result.error());
+ 				else
+ 					alert("Successfully");
+ 			}
+ 		);
+ 	}
+ 
+ 	function uninstallRobot()
+ 	{
+ 		BX24.callMethod(
+ 				'bizproc.robot.delete',
+ 				{
+ 					'CODE': 'bitrix-sendpulse-integration'
+ 				},
+ 				function(result)
+ 				{
+ 					if(result.error())
+ 						alert('Error: ' + result.error());
+ 					else
+ 						alert("Successfully");
+ 				}
+ 		);
+ 	}
+ 
+ 	function getList()
+ 	{
+ 
+ 		BX24.callMethod(
+ 			'bizproc.robot.list',
+ 			{},
+ 			function(result)
+ 			{
+ 				if(result.error())
+ 					alert("Error: " + result.error());
+ 				else
+ 					alert("Codes of installed robots: " + result.data().join(', '));
+ 			}
+ 		);
+ 	}
+ 
+ </script>
+ <?php else:?>
+ 	<form name="props" class="container-fluid">
+ <?php
+ $options = json_decode($_POST['PLACEMENT_OPTIONS'], true);
+
+ foreach ($options['properties'] as $id => $property)
+ {
+ 	$multiple = isset($property['MULTIPLE']) && $property['MULTIPLE'] === 'Y';
+ 	$val = (array) $options['current_values'][$id];
+ 	if (!$val)
+ 	{
+ 		$val[] = '';
+ 	}
+ 
+ 	if ($multiple)
+ 	{
+ 		$val[] = '';
+ 	}
+ 
+ 	$name = $multiple ? $id.'[]' : $id;
+ 	?>
+ 	<div class="form-group">
+ 		<label><?=htmlspecialchars($property['NAME'])?>:</label>
+ 		<?foreach ($val as $v):?>
+ 		<p><input name="<?=$name?>" value="<?=htmlspecialchars((string)$v)?>" class="form-control" onchange="setPropertyValue('<?=$id?>', this.name, <?=(int)$multiple?>)"></p>
+ 		<?endforeach;?>
+ 	</div>
+ 	<?
+ }
+ ?>
+ 		<script>
+ 			function setPropertyValue(name, inputName, multiple)
+ 			{
+ 				var form = new FormData(document.forms.props);
+ 				var value = multiple? form.getAll(inputName) : form.get(inputName);
+ 				var params = {};
+ 				params[name] = value;
+ 
+ 				BX24.placement.call(
+ 					'setPropertyValue',
+ 					params
+ 				)
+ 			}
+ 		</script>
+ 	</form>
+ <?php endif;?>
+ </body>
+</html>
